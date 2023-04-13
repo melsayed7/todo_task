087720182a3741d3c504057c29cc44bc46c9df8d
melsayed7 <mohamedelbiomy336@gmail.com>
2023-04-13 13:40:18 +0200

    add clean arch structure files

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:todo_task/login_screen.dart';
+import 'package:todo_task/presentation/pages/login_screen.dart';
+
+import 'bloc_observer.dart';
 
 void main() {
+  Bloc.observer = MyBlocObserver();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       initialRoute: LoginScreen.routeName,
       routes: {
-      LoginScreen.routeName : (_) => LoginScreen(),
+        LoginScreen.routeName : (_) => LoginScreen(),
       },
     );
   }
